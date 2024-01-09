23210268b6bcf6e7e435d2a0208776360949464b
berdikulov-571 <mygithub571@gmail.com>
2024-01-09 17:52:31 +0500

    Auditable

---
+++ src/NarkoCenter.Domain/Common/BaseEntities/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace NarkoCenter.Domain.Common.BaseEntities
+{
+    public class Auditable
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
