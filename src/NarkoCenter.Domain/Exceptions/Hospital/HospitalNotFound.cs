821b9e439c0520b549a84f362fbfbb5c8f599d13
berdikulov-571 <mygithub571@gmail.com>
2024-01-19 01:07:54 +0500

    Exception

---
+++ src/NarkoCenter.Domain/Exceptions/Hospital/HospitalNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace NarkoCenter.Domain.Exceptions.Hospital
+{
+    public class HospitalNotFound : GlobalExceptions
+    {
+        public HospitalNotFound()
+        {
+            TitleMessage = "Hospital Not Found!";
+        }
+    }
+}
