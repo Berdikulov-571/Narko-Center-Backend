476b257c972ce75a7cfa85d3e12ab63e4e86748b
berdikulov-571 <mygithub571@gmail.com>
2024-01-18 15:58:46 +0500

    Document Dto

---
+++ src/NarkoCenter.Domain/Dtos/Documents/DocumentCreateDto.cs
@@ -0,0 +1,10 @@
+﻿using NarkoCenter.Domain.Enums.DocumentTypes;
+
+namespace NarkoCenter.Domain.Dtos.Documents
+{
+    public class DocumentCreateDto
+    {
+        public string DocumentPath { get; set; }
+        public DocumentType DocumentType { get; set; }
+    }
+}
