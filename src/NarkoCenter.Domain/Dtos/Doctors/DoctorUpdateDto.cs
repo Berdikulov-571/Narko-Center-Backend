83ecb71296f0d4d732335d8ea27aa5e0b09ff03c
berdikulov-571 <mygithub571@gmail.com>
2024-01-18 15:56:57 +0500

    Doctor Dto

---
+++ src/NarkoCenter.Domain/Dtos/Doctors/DoctorUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace NarkoCenter.Domain.Dtos.Doctors
+{
+    public class DoctorUpdateDto
+    {
+        public string FullName { get; set; }
+        public string DoctorCategory { get; set; }
+        public int Experience { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
