7ef5acedc25867307e4154b7b9993c54484aa793
berdikulov-571 <mygithub571@gmail.com>
2024-01-18 15:59:02 +0500

    Help Create Dto

---
+++ src/NarkoCenter.Domain/Dtos/CallCenter/HelpCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NarkoCenter.Domain.Dtos.CallCenter
+{
+    public class HelpCreateDto
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WhoNeedsHelp { get; set; }
+        public string WhatToTreatFor { get; set; }
+    }
+}
