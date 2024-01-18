c951b00f54bc6ace3bb6a4f80be6dece2cd3f5da
berdikulov-571 <mygithub571@gmail.com>
2024-01-18 15:57:32 +0500

    Doctor

--- src/NarkoCenter.Domain/Entities/Doctors/Doctor.cs
+++ src/NarkoCenter.Domain/Entities/Doctors/Doctor.cs
@@ -7,6 +7,7 @@
         public string DoctorCategory { get; set; }
         public int Experience { get; set; }
         public string PhoneNumber { get; set; }
+        public string ImagePath { get; set; }
         public DateTime BirthDate { get; set; }
     }
 }
