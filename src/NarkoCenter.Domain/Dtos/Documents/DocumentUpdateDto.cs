c2be5f27b5548ea0f734e7fec07bbde7c80aeb1a
berdikulov-571 <mygithub571@gmail.com>
2024-01-19 01:07:29 +0500

    Document Dto

--- src/NarkoCenter.Domain/Dtos/Documents/DocumentUpdateDto.cs
+++ src/NarkoCenter.Domain/Dtos/Documents/DocumentUpdateDto.cs
@@ -1,11 +1,12 @@
-﻿using NarkoCenter.Domain.Enums.DocumentTypes;
+﻿using Microsoft.AspNetCore.Http;
+using NarkoCenter.Domain.Enums.DocumentTypes;
 
 namespace NarkoCenter.Domain.Dtos.Documents
 {
     public class DocumentUpdateDto
     {
         public int DocumentId { get; set; }
-        public string DocumentPath { get; set; }
+        public IFormFile? DocumentPath { get; set; }
         public DocumentType DocumentType { get; set; }
     }
 }
