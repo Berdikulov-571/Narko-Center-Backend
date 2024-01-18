d3b806d8cc5b65f05432a6854525c64d821b3f28
berdikulov-571 <mygithub571@gmail.com>
2024-01-18 15:59:53 +0500

    Service Dto

---
+++ src/NarkoCenter.Domain/Dtos/Services/ServiceUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace NarkoCenter.Domain.Dtos.Services
+{
+    public class ServiceUpdateDto
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; } = default!;
+        public decimal PriceADay { get; set; } = default!;
+        public int HowManyPeopleThisRoom { get; set; } = default!;
+        public int DoctorId { get; set; }
+        public string IconPath { get; set; }
+    }
+}
