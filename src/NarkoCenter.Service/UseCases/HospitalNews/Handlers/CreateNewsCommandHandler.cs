cec5ea1f618b8f6a37b75d4fb23fa3dfff54d371
berdikulov-571 <mygithub571@gmail.com>
2024-01-19 19:27:26 +0500

    Create News

---
+++ src/NarkoCenter.Service/UseCases/HospitalNews/Handlers/CreateNewsCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using NarkoCenter.Domain.Entities.HospitalNews;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.Abstractions.File;
+using NarkoCenter.Service.UseCases.HospitalNews.Commands;
+
+namespace NarkoCenter.Service.UseCases.HospitalNews.Handlers
+{
+    public class CreateNewsCommandHandler : IRequestHandler<CreateNewsCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IFileService _fileService;
+
+        public CreateNewsCommandHandler(IApplicationDbContext context, IFileService fileService)
+        {
+            _context = context;
+            _fileService = fileService;
+        }
+
+        public async Task<int> Handle(CreateNewsCommand request, CancellationToken cancellationToken)
+        {
+            News news = new News()
+            {
+                CreatedAt = DateTime.UtcNow,
+                Description = request.Description,
+                ImagePath = await _fileService.UploadImageAsync(request.ImagePath),
+            };
+
+            await _context.News.AddAsync(news);
+            int response = await _context.SaveChangesAsync(cancellationToken);
+
+            return response;
+        }
+    }
+}
