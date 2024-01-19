d0e30c0904de4834566f85378f7c8cfb257ff050
berdikulov-571 <mygithub571@gmail.com>
2024-01-19 19:26:57 +0500

    Update News Command

---
+++ src/NarkoCenter.Service/UseCases/HospitalNews/Commands/UpdateNewsCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NarkoCenter.Domain.Dtos.HospitalNews;
+
+namespace NarkoCenter.Service.UseCases.HospitalNews.Commands
+{
+    public class UpdateNewsCommand : HospitalNewsUpdateDto, IRequest<int>
+    {
+
+    }
+}
