d0204cc154b0ea7124d4e685c7be680eaa319d39
berdikulov-571 <mygithub571@gmail.com>
2024-01-19 19:27:21 +0500

    GetAll News

---
+++ src/NarkoCenter.Service/UseCases/HospitalNews/Handlers/GetAllNewsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NarkoCenter.Domain.Entities.HospitalNews;
+using NarkoCenter.Domain.Exceptions.News;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.UseCases.HospitalNews.Queries;
+
+namespace NarkoCenter.Service.UseCases.HospitalNews.Handlers
+{
+    public class GetAllNewsQueryHandler : IRequestHandler<GetAllNewsQuery, IEnumerable<News>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllNewsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<News>> Handle(GetAllNewsQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<News>? news = await _context.News.ToListAsync(cancellationToken);
+
+            if (news.Count() == 0)
+                throw new NewsNotFound();
+            return news;
+        }
+    }
+}
