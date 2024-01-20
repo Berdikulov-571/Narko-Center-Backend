1fefe11afaf643812fc17407bf6343f57c70f1d1
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 23:11:58 +0500

    Delete

---
+++ src/NarkoCenter.Service/UseCases/Users/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NarkoCenter.Domain.Entities.Users;
+using NarkoCenter.Domain.Exceptions.Users;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.UseCases.Users.Commands;
+
+namespace NarkoCenter.Service.UseCases.Users.Handlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteUserCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            User? user = await _context.Users.FirstOrDefaultAsync(x => x.UserId == request.UserId, cancellationToken);
+
+            if (user == null)
+                throw new UserNotFound();
+
+            _context.Users.Remove(user);
+            int response = await _context.SaveChangesAsync(cancellationToken);
+
+            return response;
+        }
+    }
+}
