2e805502fda32c91f78e3e378a1fe1b3d7dbeb47
berdikulov-571 <mygithub571@gmail.com>
2024-01-21 02:47:18 +0500

    Answer

--- src/NarkoCenter.Domain/Entities/CallCenter/AnswerAndQuestions.cs
+++ src/NarkoCenter.Domain/Entities/CallCenter/AnswerAndQuestions.cs
@@ -1,7 +1,10 @@
-﻿namespace NarkoCenter.Domain.Entities.CallCenter
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NarkoCenter.Domain.Entities.CallCenter
 {
     public class AnswerAndQuestions
     {
+        [Key]
         public int QuestionId { get; set; }
         public string FullName { get; set; } = default!;
         public string PhoneNumber { get; set; } = default!;
