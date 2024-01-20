b8d49213ac39e8754acd2842a2a3d8b0eb4e0d4c
berdikulov-571 <mygithub571@gmail.com>
2024-01-21 02:49:10 +0500

    Dependecy Injection

--- src/NarkoCenter.Service/DependecyInjection.cs
+++ src/NarkoCenter.Service/DependecyInjection.cs
@@ -1,7 +1,25 @@
-﻿namespace NarkoCenter.Service
-{
-    public class DependecyInjection
-    {
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NarkoCenter.Service.Abstractions.File;
+using NarkoCenter.Service.Service.Files;
+using System.Reflection;
 
+namespace NarkoCenter.Service
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddScoped<IFileService, FileService>();
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.InstanceName = "Narko.Center";
+                options.Configuration = "127.0.0.1:6379";
+            });
+
+            return services;
+        }
     }
 }
