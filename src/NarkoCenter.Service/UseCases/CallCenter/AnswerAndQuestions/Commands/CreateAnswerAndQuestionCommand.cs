c9b082380b0851827feb01db21e64a36e95ba4aa
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 23:11:03 +0500

    Create Answer

---
+++ src/NarkoCenter.Service/UseCases/CallCenter/AnswerAndQuestions/Commands/CreateAnswerAndQuestionCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NarkoCenter.Domain.Dtos.CallCenter;
+
+namespace NarkoCenter.Service.UseCases.CallCenter.AnswerAndQuestions.Commands
+{
+    public class CreateAnswerAndQuestionCommand : AnswerAndQuestionsCreateDto, IRequest<int>
+    {
+
+    }
+}
