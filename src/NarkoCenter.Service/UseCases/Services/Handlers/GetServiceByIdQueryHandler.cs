01d3dbd69a1df5a64cb86af0e9ce351735836d06
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 22:43:53 +0500

    Get Service

---
+++ src/NarkoCenter.Service/UseCases/Services/Handlers/GetServiceByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NarkoCenter.Domain.Exceptions.Services;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.UseCases.Services.Queries;
+
+namespace NarkoCenter.Service.UseCases.Services.Handlers
+{
+    public class GetServiceByIdQueryHandler : IRequestHandler<GetServiceByIdQuery, Domain.Entities.Services.Service>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetServiceByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Services.Service> Handle(GetServiceByIdQuery request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Services.Service service = await _context.Services.FirstOrDefaultAsync(x => x.ServiceId == request.ServiceId, cancellationToken);
+
+            if (service == null)
+                throw new ServiceNotFound();
+
+            return service;
+        }
+    }
+}
