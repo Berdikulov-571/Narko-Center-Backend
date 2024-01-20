fbd251074a5c6ac43b49a9ef7d482b82fb6c375f
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 23:12:38 +0500

    Get Answer

---
+++ src/NarkoCenter.Service/UseCases/CallCenter/AnswerAndQuestions/Queries/GetAnswerAndQuestionByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NarkoCenter.Domain.Entities.CallCenter;
+
+namespace NarkoCenter.Service.UseCases.CallCenter.AnswerAndQuestions.Queries
+{
+    public class GetAnswerAndQuestionByIdQuery : IRequest<AnswerAndQuestions>
+    {
+        public int AnswerAndQuestionId { get; set; }
+    }
+}
