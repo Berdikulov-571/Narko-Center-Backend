507d5cd50aed58945329e97a9dc58cac03135f29
berdikulov-571 <mygithub571@gmail.com>
2024-01-21 02:43:35 +0500

    News

---
+++ src/NarkoCenter.Api/Controllers/NewsController.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NarkoCenter.Service.UseCases.HospitalNews.Commands;
+using NarkoCenter.Service.UseCases.HospitalNews.Queries;
+
+namespace NarkoCenter.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class NewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NewsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync([FromForm] CreateNewsCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var response = await _mediator.Send(new GetAllNewsQuery());
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync([FromForm] UpdateNewsCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int newsId)
+        {
+            int response = await _mediator.Send(new DeleteNewsCommand() { NewsId = newsId });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int newsId)
+        {
+            var response = await _mediator.Send(new GetNewsByIdQuery() { NewsId = newsId });
+
+            return Ok(response);
+        }
+    }
+}
