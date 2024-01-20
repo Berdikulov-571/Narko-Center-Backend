ec176a01bf82bb495401ac2219b62968fe9c23b2
berdikulov-571 <mygithub571@gmail.com>
2024-01-21 02:46:30 +0500

    Hospital

--- src/NarkoCenter.Domain/Dtos/HospitalNews/HospitalNewsUpdateDto.cs
+++ src/NarkoCenter.Domain/Dtos/HospitalNews/HospitalNewsUpdateDto.cs
@@ -7,6 +7,5 @@ namespace NarkoCenter.Domain.Dtos.HospitalNews
         public int HospitalNewsId { get; set; }
         public string Description { get; set; }
         public IFormFile? ImagePath { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
