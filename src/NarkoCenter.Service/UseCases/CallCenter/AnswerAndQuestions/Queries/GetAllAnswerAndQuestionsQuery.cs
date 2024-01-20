7c18b2597ba0dc6d0fe794845c21f0bd553f933c
berdikulov-571 <mygithub571@gmail.com>
2024-01-21 02:46:50 +0500

    Get All

--- src/NarkoCenter.Service/UseCases/CallCenter/AnswerAndQuestions/Queries/GetAllAnswerAndQuestionsQuery.cs
+++ src/NarkoCenter.Service/UseCases/CallCenter/AnswerAndQuestions/Queries/GetAllAnswerAndQuestionsQuery.cs
@@ -3,7 +3,7 @@ using NarkoCenter.Domain.Entities.CallCenter;
 
 namespace NarkoCenter.Service.UseCases.CallCenter.AnswerAndQuestions.Queries
 {
-    public class GetAllAnswerAndQuestionsQuery : IRequest<IEnumerable<AnswerAndQuestions>>
+    public class GetAllAnswerAndQuestionsQuery : IRequest<IEnumerable<Domain.Entities.CallCenter.AnswerAndQuestions>>
     {
 
     }
