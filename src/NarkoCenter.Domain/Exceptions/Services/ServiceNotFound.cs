88b44de2bee9d8b617223ea60e583dea1f78117c
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 22:42:33 +0500

    Service Not Found

---
+++ src/NarkoCenter.Domain/Exceptions/Services/ServiceNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace NarkoCenter.Domain.Exceptions.Services
+{
+    public class ServiceNotFound : GlobalExceptions
+    {
+        public ServiceNotFound()
+        {
+            TitleMessage = "Service Not Found!";
+        }
+    }
+}
