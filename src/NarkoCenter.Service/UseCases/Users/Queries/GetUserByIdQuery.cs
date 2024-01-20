267ca92389155f2ebc9f580d54bfad043edefd8b
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 23:14:29 +0500

    Get User

---
+++ src/NarkoCenter.Service/UseCases/Users/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NarkoCenter.Domain.Entities.Users;
+
+namespace NarkoCenter.Service.UseCases.Users.Queries
+{
+    public class GetUserByIdQuery : IRequest<User>
+    {
+        public int UserId { get; set; }
+    }
+}
