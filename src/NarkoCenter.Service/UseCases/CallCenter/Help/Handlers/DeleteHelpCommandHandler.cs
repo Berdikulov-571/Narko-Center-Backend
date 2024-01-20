c9b6c5ac61546c78162b17bd66e022249c20333e
berdikulov-571 <mygithub571@gmail.com>
2024-01-21 02:48:32 +0500

    Delete

---
+++ src/NarkoCenter.Service/UseCases/CallCenter/Help/Handlers/DeleteHelpCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NarkoCenter.Domain.Exceptions.Help;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.UseCases.CallCenter.Help.Commands;
+
+namespace NarkoCenter.Service.UseCases.CallCenter.Help.Handlers
+{
+    public class DeleteHelpCommandHandler : IRequestHandler<DeleteHelpCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteHelpCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(DeleteHelpCommand request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.CallCenter.Help? help = await _context.Helps.FirstOrDefaultAsync(x => x.HelpId == request.HelpId,cancellationToken);
+
+            if (help == null)
+                throw new HelpNotFound();
+
+            _context.Helps.Remove(help);
+            int response = await _context.SaveChangesAsync(cancellationToken);
+
+            return response;
+        }
+    }
+}
