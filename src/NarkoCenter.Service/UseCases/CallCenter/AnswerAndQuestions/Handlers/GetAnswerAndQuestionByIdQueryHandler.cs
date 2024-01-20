f0fd4a540021ddd6dabe4cd56af5131229407f42
berdikulov-571 <mygithub571@gmail.com>
2024-01-20 23:12:30 +0500

    Getl All Answer

---
+++ src/NarkoCenter.Service/UseCases/CallCenter/AnswerAndQuestions/Handlers/GetAnswerAndQuestionByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NarkoCenter.Domain.Entities.CallCenter;
+using NarkoCenter.Domain.Exceptions.AnswerAndQuestions;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.UseCases.CallCenter.AnswerAndQuestions.Queries;
+
+namespace NarkoCenter.Service.UseCases.CallCenter.AnswerAndQuestions.Handlers
+{
+    public class GetAnswerAndQuestionByIdQueryHandler : IRequestHandler<GetAnswerAndQuestionByIdQuery, AnswerAndQuestions>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAnswerAndQuestionByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AnswerAndQuestions> Handle(GetAnswerAndQuestionByIdQuery request, CancellationToken cancellationToken)
+        {
+            AnswerAndQuestions? answerAndQuestions = await _context.AnswerAndQuestions.FirstOrDefaultAsync(x => x.QuestionId == request.AnswerAndQuestionId);
+
+            if (answerAndQuestions == null)
+                throw new QuestionNotFound();
+            return answerAndQuestions;
+        }
+    }
+}
