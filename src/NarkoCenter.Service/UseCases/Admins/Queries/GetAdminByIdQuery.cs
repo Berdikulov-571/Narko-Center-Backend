5bdef717b33bce767e771e436c878a72c9dd55df
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:52:07 +0500

    Get

---
+++ src/NarkoCenter.Service/UseCases/Admins/Queries/GetAdminByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NarkoCenter.Domain.Entities.Admins;
+
+namespace NarkoCenter.Service.UseCases.Admins.Queries
+{
+    public class GetAdminByIdQuery : IRequest<Admin>
+    {
+        public int AdminId { get; set; }
+    }
+}
