7f32dfe0dbce0e236668a82497a940826e97edad
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:51:52 +0500

    Create

---
+++ src/NarkoCenter.Service/UseCases/Admins/Handlers/CreateAdminCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NarkoCenter.Domain.Entities.Admins;
+using NarkoCenter.Domain.Exceptions.Admins;
+using NarkoCenter.Service.Abstractions.DataAccess;
+using NarkoCenter.Service.Security;
+using NarkoCenter.Service.UseCases.Admins.Commands;
+
+namespace NarkoCenter.Service.UseCases.Admins.Handlers
+{
+    public class CreateAdminCommandHandler : IRequestHandler<CreateAdminCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateAdminCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateAdminCommand request, CancellationToken cancellationToken)
+        {
+            Admin checkAdmin = await _context.Admins.FirstOrDefaultAsync(x => x.Email == request.Email, cancellationToken);
+
+            if (checkAdmin != null)
+                throw new AdminAlreadyExists();
+
+            Admin admin = new Admin()
+            {
+                Email = request.Email,
+                FullName = request.FullName,
+                PasswordHash = PasswordHash.ComputeSHA512HashFromString(request.Password),
+                Role = request.Role
+            };
+
+            await _context.Admins.AddAsync(admin);
+            int response = await _context.SaveChangesAsync(cancellationToken);
+
+            return response;
+        }
+    }
+}
