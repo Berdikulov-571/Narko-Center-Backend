1c3c0f6cd0fda06ccec54478f7b7bfb405b94f3f
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:53:22 +0500

    AdminNOtFound

---
+++ src/NarkoCenter.Domain/Exceptions/Admins/AdminNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace NarkoCenter.Domain.Exceptions.Admins
+{
+    public class AdminNotFound : GlobalExceptions
+    {
+        public AdminNotFound()
+        {
+            TitleMessage = "Admin Not Found!";
+        }
+    }
+}
