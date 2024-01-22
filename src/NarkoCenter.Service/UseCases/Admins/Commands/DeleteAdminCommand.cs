116f5a989fc94e197aa62eb58613e9b241173fa7
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:51:46 +0500

    Delete

---
+++ src/NarkoCenter.Service/UseCases/Admins/Commands/DeleteAdminCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace NarkoCenter.Service.UseCases.Admins.Commands
+{
+    public class DeleteAdminCommand : IRequest<int>
+    {
+        public int AdminId { get; set; }
+    }
+}
