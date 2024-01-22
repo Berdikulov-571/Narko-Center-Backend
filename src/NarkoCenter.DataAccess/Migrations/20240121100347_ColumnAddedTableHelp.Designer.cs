d2f52c5b774feaf297fb757ebe84fdba0cafb826
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:50:39 +0500

    MIgrations

--- src/NarkoCenter.DataAccess/Migrations/20240121100347_ColumnAddedTableHelp.Designer.cs
+++ src/NarkoCenter.DataAccess/Migrations/20240121100347_ColumnAddedTableHelp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NarkoCenter.DataAccess.Persistence.DataAccess;
 
@@ -11,9 +12,11 @@ using NarkoCenter.DataAccess.Persistence.DataAccess;
 namespace NarkoCenter.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240121100347_ColumnAddedTableHelp")]
+    partial class ColumnAddedTableHelp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
