0b1d2c308f57db39df4a2819843500195398c4d7
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:51:39 +0500

    Admin

---
+++ src/NarkoCenter.Domain/Entities/Admins/Admin.cs
@@ -0,0 +1,15 @@
+﻿using NarkoCenter.Domain.Enums.Roles;
+using System.ComponentModel.DataAnnotations;
+
+namespace NarkoCenter.Domain.Entities.Admins
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public Role Role { get; set; }
+    }
+}
