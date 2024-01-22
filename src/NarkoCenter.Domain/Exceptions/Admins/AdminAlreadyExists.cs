e8346dbbc0c677d8f988befb2518c5edb255e93f
berdikulov-571 <mygithub571@gmail.com>
2024-01-22 14:52:44 +0500

    Admin

---
+++ src/NarkoCenter.Domain/Exceptions/Admins/AdminAlreadyExists.cs
@@ -0,0 +1,10 @@
+﻿namespace NarkoCenter.Domain.Exceptions.Admins
+{
+    public class AdminAlreadyExists : GlobalExceptions
+    {
+        public AdminAlreadyExists()
+        {
+            TitleMessage = "Admin Already Exists!";
+        }
+    }
+}
